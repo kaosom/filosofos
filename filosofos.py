1e50a41590ca50013777371ad4420aec20c1e45b
= <=>
2023-10-17 20:17:07 -0600

    Se quita la izquierda y derecha asi como cambien se ponen algunas interfaces

--- filosofos.py
+++ filosofos.py
@@ -1,40 +1,69 @@
-import threading
+from threading import Thread, Semaphore
+import random
 import time
 
-# Definimos una clase llamada "Philosopher" que representa a un filósofo.
-class Philosopher(threading.Thread):
-    def __init__(self, id, left, right, butler):  # Cada filósofo tiene un número de identificación y dos palillos (izquierdo y derecho).
-        threading.Thread.__init__(self)
-        self.id = id  # Guardamos el número de identificación del filósofo.
-        self.left = left  # Guardamos el palillo izquierdo.
-        self.right = right  # Guardamos el palillo derecho.
-        self.butler = butler  # Usamos un "butler" (mayordomo) para controlar el acceso de los filósofos a la mesa.
+class DiningPhilosophers:
+    def __init__(self, number_of_philosophers, meal_size=9):
+        self.meals = [meal_size for _ in range(number_of_philosophers)]
+        self.chopsticks = [Semaphore(value=1) for _ in range(number_of_philosophers)]
+        self.status = ['  P  ' for _ in range(number_of_philosophers)]
+        self.chopstick_holders = ['  ' for _ in range(number_of_philosophers)]
+        self.number_of_philosophers = number_of_philosophers
 
-    def run(self):  # Esta función representa lo que hace un filósofo cuando comienza a "pensar" y "comer."
-        for _ in range(1):  # Cada filósofo "piensa" y "come" 1 vez.
-            self.butler.acquire()  # El filósofo adquiere el permiso del mayordomo para sentarse en la mesa.
-            print(f"Filosofo {self.id} está pensando\n")  # Mostramos un mensaje de que el filósofo está pensando.
-            time.sleep(2)  # Esperamos un poquito (0.1 segundos) para simular el pensamiento.
-            self.left.acquire()  # El filósofo adquiere el palillo izquierdo.
-            print(f"El filósofo {self.id} tomó el tenedor izquierdo.\n")
-            time.sleep(2)  # Esperamos otro poquito para simular el proceso de tomar el palillo.
-            self.right.acquire()  # El filósofo adquiere el palillo derecho.
-            print(f"El filósofo {self.id} tomó el tenedor derecho.\n")
-            print(f"Filosofo {self.id} está comiendo")  # Mostramos un mensaje de que el filósofo está comiendo.
-            time.sleep(2)  # Esperamos un poquito para simular la comida.
-            self.right.release()  # El filósofo libera el palillo derecho.
-            self.left.release()  # El filósofo libera el palillo izquierdo.
-            self.butler.release()  # El filósofo libera el permiso del mayordomo para que otro pueda sentarse a la mesa.
-        print(f"El filósofo {self.id} ha terminado de pensar y comer\n")  # Mostramos un mensaje cuando el filósofo termina.
+    def philosopher(self, i):
+        j = (i + 1) % self.number_of_philosophers
+        while self.meals[i] > 0:
+            self.status[i] = '  P  '
+            time.sleep(random.random())
+            self.status[i] = '  E  '
+            if self.chopsticks[i].acquire(timeout=1):
+                self.chopstick_holders[i] = '_/   '
+                time.sleep(random.random())
+                if self.chopsticks[j].acquire(timeout=1):
+                    self.chopstick_holders[i] = '_/ \\_ '
+                    self.status[i] = '  C  '
+                    time.sleep(random.random())
+                    self.meals[i] -= 1
+                    self.chopsticks[j].release()
+                    self.chopstick_holders[i] =  '_/   '
+                self.chopsticks[i].release()
+                self.chopstick_holders[i] =  '     '
+                self.status[i] = '  P  '
+            self.status[i] = '  F  ' 
+
+    def print_status(self):
+        header = "=" * (self.number_of_philosophers * 6)
+        status_str = " ".join(self.status)
+        holders_str = " ".join(self.chopstick_holders)
+        meals_str = " ".join([f"{m:3d}" for m in self.meals])
+        total_meals = str(sum(self.meals))
+        holders = [f"    {i}" for i in range(self.number_of_philosophers)]
+
+        print(header.center(len(header)))
+        print(status_str.center(len(status_str)))
+        print(holders_str.center(len(holders_str)))
+        #print(meals_str.center(len(meals_str)))
+        holders_chop = " ".join(holders)  # Muestra los identificadores de los palillos
+        print(holders_chop.center(len(holders_chop)))
+        print(f"Total Meals: {total_meals}".center(len(header)))
 
 def main():
-    num_philosophers = 5  # Hay 5 filósofos en la mesa.
-    butler = threading.Semaphore(num_philosophers - 1)  # Creamos un semáforo que controla el acceso de los filósofos.
-    chopsticks = [threading.Semaphore(1) for _ in range(num_philosophers)]  # Creamos los palillos con semáforos para que los usen los filósofos.
-    philosophers = [Philosopher(i, chopsticks[i], chopsticks[(i + 1) % num_philosophers], butler) for i in range(num_philosophers)]  # Creamos a los filósofos y les damos los palillos y el mayordomo.
+    n = 5
+    m = 1
+    dining_philosophers = DiningPhilosophers(n, m)
+    philosophers = [Thread(target=dining_philosophers.philosopher, args=(i,)) for i in range(n)]
+    random.shuffle(philosophers)
 
     for philosopher in philosophers:
-        philosopher.start()  # Hacemos que los filósofos empiecen a "pensar" y "comer."
+        philosopher.start()
 
-if __name__ == '__main__':
-    main()  # Iniciamos el programa llamando a la función "main."
+    while sum(dining_philosophers.meals) > 0:
+        dining_philosophers.print_status()
+        time.sleep(0.1)
+    dining_philosophers.print_status()
+
+    for philosopher in philosophers:
+        philosopher.join()
+
+if __name__ == "__main__":
+    main()
